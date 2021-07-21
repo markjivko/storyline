5f9cf7012e2aa5ac0abe69133bba9135e4d3b9e5
Stephino <50949149+Stephino@users.noreply.github.com>
2021-07-21 16:13:14 +0300

    [code] Added copyright and small css property bug fixed

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,13 @@
+/**
+ * Storyline
+ * 
+ * @title      Storyline Demo
+ * @desc       A short storyline demo page
+ * @copyright  (c) 2013, Mark Jivko
+ * @author     Mark Jivko <stephino.team@gmail.com>
+ * @package    stephino-rpg
+ * @license    GPL v3+, https://gnu.org/licenses/gpl-3.0.txt
+ */
 $(document).ready(function() {
     // Cache objects
     var objectsCache = {
@@ -78,3 +88,5 @@ $(document).ready(function() {
         });
     }
 });
+
+/*EOF*/
